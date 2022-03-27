4c53dd23a8df15c75e687c76711e158e65c0b8aa
OliveriQ <oliverilucas20@gmail.com>
2022-03-27 13:31:12 +0100

    implemented basic FEN parsing

--- src/main.go
+++ src/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Print("Go chess engine\n\n")
 	initialize_lookup_tables()
-	
+
+	parse_fen(start_position, 0)
 	print_board()
+
 }
