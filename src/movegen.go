18482c7efa9d7ef5fc8bbee26d74c4f2600964d3
OliveriQ <76691188+OliveriQ@users.noreply.github.com>
2022-03-30 08:29:22 +0100

    Update movegen.go

--- src/movegen.go
+++ src/movegen.go
@@ -5,7 +5,7 @@ import "fmt"
 
 // lookup table for knight attacks
 var KNIGHT_ATTACKS_TABLE = [64]uint64 {
-	0x0000000000020400, 0x0000000000050800, 0x00000000000A1100, 0x0000000000142200,
+    0x0000000000020400, 0x0000000000050800, 0x00000000000A1100, 0x0000000000142200,
     0x0000000000284400, 0x0000000000508800, 0x0000000000A01000, 0x0000000000402000,
     0x0000000002040004, 0x0000000005080008, 0x000000000A110011, 0x0000000014220022,
     0x0000000028440044, 0x0000000050880088, 0x00000000A0100010, 0x0000000040200020,
@@ -25,7 +25,7 @@ var KNIGHT_ATTACKS_TABLE = [64]uint64 {
 
 // lookup table for king attacks
 var KING_ATTACKS_TABLE = [64]uint64 {
-	0x0000000000000302, 0x0000000000000705, 0x0000000000000E0A, 0x0000000000001C14,
+    0x0000000000000302, 0x0000000000000705, 0x0000000000000E0A, 0x0000000000001C14,
     0x0000000000003828, 0x0000000000007050, 0x000000000000E0A0, 0x000000000000C040,
     0x0000000000030203, 0x0000000000070507, 0x00000000000E0A0E, 0x00000000001C141C,
     0x0000000000382838, 0x0000000000705070, 0x0000000000E0A0E0, 0x0000000000C040C0,
@@ -85,113 +85,6 @@ var BLACK_PAWN_ATTACKS = [64]uint64 {
 
 // lookup table for pawn attacks
 var PAWN_ATTACKS_TABLE [2][64]uint64
-
-// function for encoding a move
-func encode_move(source, target, piece, promoted, capture, double, enpassant, castling int) int {
-	return ((source)         |          
-		   (target << 6)     |     
-		   (piece << 12)     |     
-		   (promoted << 16)  |  
-		   (capture << 20)   |   
-		   (double << 21)    |    
-		   (enpassant << 22) | 
-		   (castling << 23))
-}
-
-// extract source square
-func get_move_source(move int) int {
-	return move & 0x3f
-}
-
-// extract target square
-func get_move_target(move int) int {
-	return (move & 0xfc0) >> 6
-}
-
-// extract piece 
-func get_move_piece(move int) int {
-	return (move & 0xf000) >> 12
-}
-
-// extract promoted piece
-func get_move_promoted(move int) int {
-	return (move & 0xf0000) >> 16
-} 
-
-// extract capture flag
-func get_move_capture(move int) int {
-	return move & 0x100000
-}
-
-// extract double pawn push flag
-func get_move_double(move int) int {
-	return move & 0x200000
-}
-
-// extract enpassant flag
-func get_move_enpassant(move int) int {
-	return move & 0x400000
-}
-
-// extract castling flag
-func get_move_castling(move int) int {
-	return move & 0x800000
-}
-
-// Move list structure
-type Moves struct {
-	// move list
-	moves [256]int
-
-	// pointer to current move
-	count int
-}
-
-// add move to move list
-func add_move(move_list *Moves, move int) {
-	// store move
-	move_list.moves[move_list.count] = move
-
-	// increment pointer
-	move_list.count++
-}
-
-// print move (for UCI purposes)
-func print_move(move int) {
-	if get_move_promoted(move) > 0 {
-	fmt.Print(square_to_coordinates[get_move_source(move)],
-			  square_to_coordinates[get_move_target(move)],
-		      promoted_pieces[get_move_promoted(move)])
-	} else {
-		fmt.Print(square_to_coordinates[get_move_source(move)],
-				  square_to_coordinates[get_move_target(move)])
-	}
-}
-
-// print move list (for debuggin purposes)
-func print_move_list(move_list Moves) {
-	if move_list.count == 0 {
-		fmt.Print("\nNo moves in move list\n")
-	} else {
-		fmt.Print("\n     move    piece     capture   double    enpass    castling\n\n")
-		for move_count := 0; move_count < move_list.count; move_count++ {
-			var move int = move_list.moves[move_count]
-
-			fmt.Print("     ", square_to_coordinates[get_move_source(move)], square_to_coordinates[get_move_target(move)])
-			if get_move_promoted(move) > 0 { fmt.Printf("%c", promoted_pieces[get_move_promoted(move)]) } else { fmt.Print(" ") }
-			fmt.Printf("     %c", ascii_pieces[get_move_piece(move)])
-			if get_move_capture(move) > 0 { fmt.Print("          1") } else { fmt.Print("          0") }
-			if get_move_double(move) > 0 { fmt.Print("        1") } else { fmt.Print("        0") }
-			if get_move_enpassant(move) > 0 { fmt.Print("         1") } else { fmt.Print("         0") }
-			if get_move_castling(move) > 0 { fmt.Print("          1") } else { fmt.Print("          0") }
-
-		}
-	}
-
-	fmt.Print("\n\n     Total number of moves: ", move_list.count, "\n\n")
-}
-
-
 
 // slider pseudo move generation (hyperbola quintessence)
 func hyp_quint(square int, occ uint64, mask uint64) uint64 {
