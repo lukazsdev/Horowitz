b13a927188f001aef47e90df983e884ff95241f3
OliveriQ <oliverilucas20@gmail.com>
2022-04-13 15:48:01 +0100

    removed fens in perft

--- engine/perft.go
+++ engine/perft.go
@@ -5,25 +5,6 @@ import (
 	"time"
 )
 
-const (
-	// FEN dedug positions (perft testing)
-	empty_board = "8/8/8/8/8/8/8/8 b - - "
-	start_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 "
-	tricky_position = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 "
-	killer_position = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
-	cmk_position = "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 "
-	repetitions = "2r3k1/R7/8/1R6/8/8/P4KPP/8 w - - 0 40 "
-
-	// more positions from https://www.chessprogramming.org/Perft_Results
-	p3 = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - "
-	p4 = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
-	p5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  " 
-	p6 = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 "
-
-	// mate in 6 queen sacrifice position
-	p7 = "2kr2nr/1pp2ppp/3b4/1P3q2/2Pp1B2/5Q1P/RP3PP1/R5K1 w - - 5 18"
-)
-
 type Perft struct {
 	nodes      uint64
 	captures   uint64
