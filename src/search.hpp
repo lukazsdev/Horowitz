242e5f0a6a1063f490682ce6ec15c5394bef180f
OliveriQ <oliverilucas20@gmail.com>
2022-05-02 10:06:18 +0100

    minor changes

--- src/search.hpp
+++ src/search.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <chrono>
 #include "chess.hpp"
 #include "search.hpp"
 #include "evaluate.hpp"
@@ -8,6 +9,7 @@
 static constexpr int maxPly = 64;
 static constexpr int infinity = 10000;
 static constexpr int checkmate = 9000;
+static constexpr int windowSize = 50;
 
 class Search {
 public:
@@ -114,8 +116,18 @@ int Search::negamax(Position pos, int alpha, int beta, int depth) {
     return alpha;
 }
 
+// root search function (iterative deepening search)
 template<Color c> 
 void Search::search(Position pos, int depth) {
+    // start search timer
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    // initialize best move to null (no move)
+    Move *bestMove = NULL;
+
+    // last iteration score
+    int lastScore = 0;
+
     // reset search info
     nodes = 0;
     ply = 0;
@@ -125,19 +137,65 @@ void Search::search(Position pos, int depth) {
     memset(pvLength, 0, sizeof(pvLength));
     memset(pvTable, 0, sizeof(pvTable));
 
+    // initialize alpha beta bounds
+    int alpha = -infinity;
+    int beta = infinity;
 
-    int score = negamax<c>(pos, -infinity, infinity, depth);
-    Move bestMove = pvTable[0][0];
+    // iterative deepening loop
+    for (int currentDepth = 1; currentDepth < depth; currentDepth++) {
+        // enable follow PV line flag
+        followPV = 1;
 
-    // loop over pv line
-    std::cout << "pv: ";
-    for (int i = 0; i < pvLength[0]; i++) {
-        // print move
-        std::cout << pvTable[0][i].toUci() << " ";
+        // search for best move within position
+        int score = negamax<c>(pos, -infinity, infinity, depth);
+
+        // get cumulative search time
+        auto t2 = std::chrono::high_resolution_clock::now();
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+
+        // adjust aspiration window technique
+        if ((score <= alpha) || (score >= beta)) {
+            alpha = -infinity;
+            beta  = infinity;    
+            currentDepth--;
+            continue;
+        }
+
+        alpha = score - windowSize;
+        beta = score + windowSize;
+
+        // save current best move
+        bestMove = &pvTable[0][0];
+
+        // if PV is available (print search info)
+        if (pvLength[0] > 0) {
+            if (score > -checkmate && score < -(checkmate-100)) {
+                std::cout << "info score mate " << -(score + checkmate) / 2 - 1 << " depth " << currentDepth;
+                std::cout << " nodes " << nodes << " time " << ms.count() << " pv ";
+            }
+            else if (score > (checkmate-100) && score < checkmate) {
+                std::cout << "info score mate " << (checkmate - score) / 2 + 1 << " depth " << currentDepth;
+                std::cout << " nodes " << nodes << " time " << ms.count() << " pv ";
+            }
+            else {
+                std::cout << "info score cp " << score << " depth " << currentDepth;
+                std::cout << " nodes " << nodes << " time " << ms.count() << " pv ";
+            }
+        }
+
+        // loop over pv line
+        std::cout << "pv: ";
+        for (int i = 0; i < pvLength[0]; i++) {
+            // print move
+            std::cout << pvTable[0][i].toUci() << " ";
+        }
+        std::cout << std::endl;
+
+        // set previous score to current score
+        lastScore = score;
     }
-    std::cout << std::endl;
 
-    std::cout << "bestmove " << bestMove.toUci() << std::endl;
+    std::cout << "bestmove " << bestMove->toUci() << std::endl;
 }
 
 
