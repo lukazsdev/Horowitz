0d7a86e9ea22a5d75a7e802f3273717b9b081b42
OliveriQ <76691188+OliveriQ@users.noreply.github.com>
2022-05-25 09:22:20 +0100

    Update timemanager.cpp

--- src/timemanager.cpp
+++ src/timemanager.cpp
@@ -16,6 +16,8 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+// https://github.com/algerbrex/blunder/blob/main/engine/timemanager.go
+
 #include "timemanager.h"
 
 void TimeManager::Start() {
