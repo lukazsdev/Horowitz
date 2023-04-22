10aa9fbe4ffeaa349c69f90c2f72adf9306aafe3
zed118 <oliverilucas20@gmail.com>
2023-04-22 22:43:29 +0100

    MIT License

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Only builds for windows are available in the Releases page. If you want to build
 * [Chess Programming Wiki](https://www.chessprogramming.org/Main_Page)
 * [Bruce Moreland articles](https://web.archive.org/web/20071026090003/http://www.brucemo.com/compchess/programming/index.htm)
 * [Bitboard chess engine in C](https://www.youtube.com/playlist?list=PLmN0neTso3Jxh8ZIylk74JpwfiWNI76Cs)
-* [Blunder 7.6.0 chess engine](https://github.com/algerbrex/blunder)
+* [Blunder 7.6.0 chess engine in Go](https://github.com/algerbrex/blunder)
--- src/uci.cpp
+++ src/uci.cpp
@@ -1,21 +1,3 @@
-/*
-  Horowitz, a UCI compatible chess engine. 
-  Copyright (C) 2022 by OliveriQ.
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "uci.h"
 #include "zobrist.h"
 #include "tt.h"
@@ -33,7 +15,7 @@ void UCIInterface::UCILoop() {
         iss >> command;
         if (command == "uci") {
             std::cout << "id name " << "Horowitz" << std::endl;
-            std::cout << "id author " << "OliveriQ" << std::endl;
+            std::cout << "id author " << "Lucas (zed118)" << std::endl;
             std::cout << "uciok" << std::endl;
         }
         else if (command == "isready") {
